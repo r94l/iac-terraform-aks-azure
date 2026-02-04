5feb8c6db408c731bc8424e6c2f6d010893de9a6
Raheem <Raheem.shonubi@gmail.com>
2026-02-04 11:58:45 +0100

    Completed Main.tf in dev

--- dev/main.tf
+++ dev/main.tf
@@ -22,3 +22,51 @@ resource "azurerm_role_assignment" "rolespn" {
     module.ServicePrincipal
   ]
 }
+
+module "keyvault" {
+  source                      = "../modules/keyvault"
+  keyvault_name               = var.keyvault_name
+  location                    = var.location
+  resource_group_name         = var.rgname
+  service_principal_name      = var.service_principal_name
+  service_principal_object_id = module.ServicePrincipal.service_principal_object_id
+  service_principal_tenant_id = module.ServicePrincipal.service_principal_tenant_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = module.ServicePrincipal.client_id
+  value        = module.ServicePrincipal.client_secret
+  key_vault_id = module.keyvault.keyvault_id
+
+  depends_on = [
+    module.keyvault
+  ]
+}
+
+#create Azure Kubernetes Service
+module "aks" {
+  source                 = "../modules/aks/"
+  service_principal_name = var.service_principal_name
+  client_id              = module.ServicePrincipal.client_id
+  client_secret          = module.ServicePrincipal.client_secret
+  location               = var.location
+  resource_group_name    = var.rgname
+  cluster_name = var.cluster_name
+  node_pool_name = var.node_pool_name
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [module.aks]
+  filename     = "./kubeconfig"
+  content      = module.aks.config
+  
+}
