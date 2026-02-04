cab944aaf088fb337dc102f22a57dc5bbeec2b32
Raheem <Raheem.shonubi@gmail.com>
2026-02-04 12:10:33 +0100

    Changes to staging

---
+++ staging/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rgname
+  location = var.location
+}
+
+module "ServicePrincipal" {
+  source                 = "../modules/ServicePrincipal"
+  service_principal_name = var.service_principal_name
+
+
+  depends_on = [
+    azurerm_resource_group.rg1
+  ]
+}
+
+resource "azurerm_role_assignment" "rolespn" {
+
+  scope                = "/subscriptions/${var.SUB_ID}"
+  role_definition_name = "Contributor"
+  principal_id         = module.ServicePrincipal.service_principal_object_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
